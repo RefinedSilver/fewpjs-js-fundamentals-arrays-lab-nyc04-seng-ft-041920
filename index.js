927422c18cf9115c10a951c270bda8475c1b1d8c
github username <github email address>
2020-06-21 12:01:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+let append = ["Milo", "Otis", "Garfield"]
+
+let prepend = ["Milo", "Otis", "Garfield"]
+
+let removeLast = ["Milo", "Otis", "Garfield"]
+
+let removeFirst = ["Milo", "Otis", "Garfield"]
+
+append.push("Odie")
+
+prepend.unshift("Odie")
+
+removeLast.pop()
+
+removeFirst.shift()
 // Write your solution here!
